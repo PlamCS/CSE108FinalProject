fc293702aeedf657df85b6c02abb583fe21a0723
Psalm Bautista <psalm.bautista13524@gmail.com>
2024-05-06 20:46:59 -0700

    admin

--- server.py
+++ server.py
@@ -32,7 +32,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(80), nullable=False)
-    role = db.Column(db.String(20), nullable=False, default='user')
+    role = db.Column(db.String(20), nullable=False, default='admin')
     @property
     def is_active(self):
         return True
